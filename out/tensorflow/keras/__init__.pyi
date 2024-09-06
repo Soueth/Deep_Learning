f529a259cf19db7ba924251da8fa48b268b11699
Soueth <julian@appmarketing.com>
2024-09-06 17:08:40 -0300

    feat(classification-regression): adiciona classificação e regressão

---
+++ out/tensorflow/keras/__init__.pyi
@@ -0,0 +1,21 @@
+from keras._tf_keras.keras import backend as backend, layers as layers, losses as losses, metrics as metrics, preprocessing as preprocessing
+from keras.api import activations as activations, applications as applications, callbacks as callbacks, config as config, constraints as constraints, datasets as datasets, distribution as distribution, dtype_policies as dtype_policies, export as export, initializers as initializers, legacy as legacy, mixed_precision as mixed_precision, models as models, ops as ops, optimizers as optimizers, quantizers as quantizers, random as random, regularizers as regularizers, tree as tree, utils as utils
+from keras.src.backend.common.keras_tensor import KerasTensor as KerasTensor
+from keras.src.backend.common.stateless_scope import StatelessScope as StatelessScope
+from keras.src.backend.common.symbolic_scope import SymbolicScope as SymbolicScope
+from keras.src.backend.exports import Variable as Variable, device as device, name_scope as name_scope
+from keras.src.dtype_policies.dtype_policy import DTypePolicy as DTypePolicy, FloatDTypePolicy as FloatDTypePolicy
+from keras.src.initializers.initializer import Initializer as Initializer
+from keras.src.layers.core.input_layer import Input as Input
+from keras.src.layers.input_spec import InputSpec as InputSpec
+from keras.src.layers.layer import Layer as Layer
+from keras.src.losses.loss import Loss as Loss
+from keras.src.metrics.metric import Metric as Metric
+from keras.src.models.model import Model as Model
+from keras.src.models.sequential import Sequential as Sequential
+from keras.src.ops.function import Function as Function
+from keras.src.ops.operation import Operation as Operation
+from keras.src.optimizers.optimizer import Optimizer as Optimizer
+from keras.src.quantizers.quantizers import Quantizer as Quantizer
+from keras.src.regularizers.regularizers import Regularizer as Regularizer
+from keras.src.version import __version__ as __version__, version as version
